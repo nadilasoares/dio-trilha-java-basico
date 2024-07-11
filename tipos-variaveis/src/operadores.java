d9867a4a39183a2f79be37e7e1145325ceeb70a2
nadilasoares <nadila202soares@gmail.com>
2024-07-11 12:12:45 -0300

    Exemplo operadores relacionais

--- tipos-variaveis/src/operadores.java
+++ tipos-variaveis/src/operadores.java
@@ -1,9 +1,16 @@
 public class operadores {
     public static void main(String[] args){
-       int a, b;
-       a=6;
-       b=6;
-       String resultado = a==b ? "verdadeiro" : "Falso";
-       System.out.println(resultado);
+       int numero1 = 1;
+       int numero2 = 2;
+
+       boolean simNao = numero1 == numero2;
+       System.out.println("Numero 1 é igual o numero 2? "+simNao);
+
+       simNao = numero1 != numero2;
+       System.out.println("Numero 1 é diferente de numero 2? "+simNao);
+
+       simNao = numero1 > numero2;
+       System.out.println("Numero 1 é maior que numero 2? "+simNao);
+       
     }
 }
