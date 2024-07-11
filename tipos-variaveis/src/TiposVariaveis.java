db1a4d7188c1608fcb0e6d0f42ac0ab86798380d
nadilasoares <nadila202soares@gmail.com>
2024-07-11 09:55:20 -0300

    Exemplos de tipos de variáveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.01; 
+
+        //casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        
+        final double valorDePI= 3.14; //garante que a variavel não mude o valor
+        
+    }
+}
