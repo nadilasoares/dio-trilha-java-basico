641916984649325165c9fe01cff9c33561736462
nadilasoares <nadila202soares@gmail.com>
2024-07-11 11:58:44 -0300

    Exemplo de uso de operador

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,9 @@
+public class operadores {
+    public static void main(String[] args){
+       int a, b;
+       a=6;
+       b=6;
+       String resultado = a==b ? "verdadeiro" : "Falso";
+       System.out.println(resultado);
+    }
+}
